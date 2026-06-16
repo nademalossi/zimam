01df90c588d8be306e1c786b82a7b539023703e1
nadeem Al-aloosi <alaloosinadeem@gmail.com>
2026-06-16 12:45:29 +0200

    تعديل ملف قاعدة البيانات وحذف  اهمال اضافة ملف env

--- zimam/src/views/productsView.vue
+++ zimam/src/views/productsView.vue
@@ -4,7 +4,7 @@ import ProductsTabel from '@/components/ProductsTabel.vue';
 import pagesheader from '@/components/pagesheader.vue';
 import popupModel from '@/components/popupModel.vue';
 import { useProductStore } from '@/stores/useProductStore';
-import { ref, onMounted,Teleport } from 'vue';
+import { ref, onMounted, Teleport } from 'vue';
 
 let products = useProductStore();
 let toggleModal = ref(false);
@@ -13,17 +13,17 @@ onMounted(() => {
     products.fetchProducts()
 
 })
-function setProductId(id){
-        if (!toggleModal.value){
+function setProductId(id) {
+    if (!toggleModal.value) {
         trackerproductId.value = id
         console.log(trackerproductId.value)
-    } else{
+    } else {
         trackerproductId.value = ""
         console.log(trackerproductId.value)
     }
 
 }
-function toggleDeleteModal(id){
+function toggleDeleteModal(id) {
     setProductId(id)
     toggleModal.value = !toggleModal.value;
 }
@@ -48,20 +48,22 @@ function deleteById(id: string) {
             </Basebutton>
         </template>
     </pagesheader>
-<Teleport to="body">
-    <popupModel :showingModal="toggleModal" @closeModal="toggleModal = false">
-      <template #header>
-       هل تريد فعلا حذف سلعة:
-      </template>
-      <template #default>
-        <p>اسم السلعة المحذوفة</p>
-      </template>
-      <template #footer>
-          <button class="bg-red-500 hover:bg-red-600 rounded-md cursor-pointer p-1 ml-5" @click="deleteById(trackerproductId); toggleDeleteModal()" >حذف</button>
-          <button class="bg-green-500 hover:bg-green-600 rounded-md cursor-pointer p-1 ml-5" @click="toggleModal = false">إلغاء</button>
-    </template>
-    </popupModel>
-</Teleport>
+    <Teleport to="body">
+        <popupModel :showingModal="toggleModal" @closeModal="toggleModal = false">
+            <template #header>
+                هل تريد فعلا حذف سلعة:
+            </template>
+            <template #default>
+                <p>اسم السلعة المحذوفة</p>
+            </template>
+            <template #footer>
+                <button class="bg-red-500 hover:bg-red-600 rounded-md cursor-pointer p-1 ml-5"
+                    @click="deleteById(trackerproductId);">حذف</button>
+                <button class="bg-green-500 hover:bg-green-600 rounded-md cursor-pointer p-1 ml-5"
+                    @click="toggleModal = false">إلغاء</button>
+            </template>
+        </popupModel>
+    </Teleport>
     <ProductsTabel :products="products.products">
         <template #setting-btn="{ productId }">
             <Basebutton :link="/edit-product/ + productId">

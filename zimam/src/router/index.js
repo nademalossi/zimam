9ca29d92bc769ea61dfe99169f006c5a6113c408
nadeem Al-aloosi <alaloosinadeem@gmail.com>
2026-07-06 16:47:46 +0200

    refactor(router): enforce kebab-case convention for all route names

--- zimam/src/router/index.js
+++ zimam/src/router/index.js
@@ -15,12 +15,12 @@ const router = createRouter({
     },
     {
       path: "/edit-product/:id",
-      name: "EditProducts",
+      name: "edit-product",
       component: () => import("@/views/EditProduct.vue"),
     },
     {
       path: "/new-product",
-      name: "newproducts",
+      name: "new-product",
       component: () => import("@/views/NewProduct.vue"),
     },
     {
@@ -40,7 +40,7 @@ const router = createRouter({
     },
     {
       path: "/view-archived-invoice/:id",
-      name: "viewarchivedinvoice",
+      name: "view-archived-invoice",
       component: () => import("@/views/ArchivedIInvoiceView.vue"),
     },
   ],
